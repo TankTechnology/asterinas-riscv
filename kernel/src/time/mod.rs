7b750673278935f434dcca326f7de7fa23ef6bfa
TankTechnology <2541826291@qq.com>
2026-09-04 08:13:51 +0800

    Add RISC-V high-resolution timer deadlines

--- kernel/src/time/mod.rs
+++ kernel/src/time/mod.rs
@@ -5,8 +5,8 @@
 pub use core::{Clock, timer};
 
 use ::core::time::Duration;
-pub use system_time::{START_TIME, SystemTime};
 pub(crate) use system_time::wall_clock_adjust_nanos;
+pub use system_time::{START_TIME, SystemTime};
 pub use timer::{Timer, TimerManager};
 
 use crate::prelude::*;
@@ -35,6 +35,7 @@ pub(super) fn init() {
 }
 
 pub(super) fn init_on_each_cpu() {
+    softirq::init_on_each_cpu();
     cpu_time_stats::init_on_each_cpu();
 }
 
