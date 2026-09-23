18aae4eb47ffb4347822b2ec3db6d9b8d6e96ae5
TankTechnology <2541826291@qq.com>
2026-09-24 05:06:35 +0800

    Fix x86_64 build for RISC-V-only device modules

--- kernel/comps/usb/src/lib.rs
+++ kernel/comps/usb/src/lib.rs
@@ -20,6 +20,7 @@ macro_rules! __log_prefix {
 #[cfg_attr(not(target_arch = "riscv64"), path = "arch/other.rs")]
 mod arch;
 mod keyboard;
+#[cfg(target_arch = "riscv64")]
 mod mouse;
 
 #[init_component]
