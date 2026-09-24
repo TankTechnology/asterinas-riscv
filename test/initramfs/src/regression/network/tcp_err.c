b328deb7f54b54a5c408e049ff7dc65625707d7b
TankTechnology <2541826291@qq.com>
2026-09-24 20:43:42 +0800

    Align TCP fault regression with Linux behavior

--- test/initramfs/src/regression/network/tcp_err.c
+++ test/initramfs/src/regression/network/tcp_err.c
@@ -505,35 +505,8 @@ FN_TEST(sendmsg_and_recvmsg)
 	msg.msg_iovlen = 2;
 
 	char recv_buffer[4096] = { 0 };
-	/*
-	 * FIXME: Linux TCP reports `EFAULT` if a copy fault occurs before
-	 * completing the first SKB touched by the call. Later faults may return
-	 * a short count at SKB granularity. Asterinas has no SKBs, so it reports
-	 * byte-granular progress instead.
-	 *
-	 * Reference:
-	 * <https://elixir.bootlin.com/linux/v7.0/source/net/ipv4/tcp.c#L1328-L1329>
-	 * <https://elixir.bootlin.com/linux/v7.0/source/net/ipv4/tcp.c#L1441-L1442>
-	 */
-#ifdef __asterinas__
-	/*
-	 * The good bytes are queued in the TCP stream and must be drained
-	 * before the next case.
-	 */
-	TEST_RES(sendmsg(sk_accepted, &msg, 0), _ret == strlen(good_buffer));
-
-	sleep(1);
-
-	iov[0].iov_base = recv_buffer;
-	iov[0].iov_len = sizeof(recv_buffer);
-	msg.msg_iovlen = 1;
-	TEST_RES(recvmsg(sk_connected, &msg, 0),
-		 _ret == strlen(good_buffer) &&
-			 memcmp(recv_buffer, good_buffer,
-				strlen(good_buffer)) == 0);
-#else
+	/* The invalid tail faults before this small TCP write is committed. */
 	TEST_ERRNO(sendmsg(sk_accepted, &msg, 0), EFAULT);
-#endif
 
 	// TEST CASE 4: Receive via a partially bad receive buffer
 
@@ -557,24 +530,7 @@ FN_TEST(sendmsg_and_recvmsg)
 	iov[1].iov_len = 1;
 	msg.msg_iovlen = 2;
 
-	/* FIXME: Refer to the above FIXME regarding sending with a bad buffer. */
-#ifdef __asterinas__
-	/*
-	 * The bytes copied are consumed from the TCP stream, so only the
-	 * remaining bytes are left for the next receive.
-	 */
-	TEST_RES(recvmsg(sk_connected, &msg, 0),
-		 _ret == 1 && recv_buffer[0] == good_buffer[1]);
-
-	memset(recv_buffer, 0, sizeof(recv_buffer));
-	iov[0].iov_base = recv_buffer;
-	iov[0].iov_len = sizeof(recv_buffer);
-	msg.msg_iovlen = 1;
-	TEST_RES(recvmsg(sk_connected, &msg, 0),
-		 _ret == strlen(good_buffer) - 2 &&
-			 memcmp(recv_buffer, good_buffer + 2,
-				strlen(good_buffer) - 2) == 0);
-#else
+	/* The fault leaves the first queued TCP segment available for retry. */
 	TEST_ERRNO(recvmsg(sk_connected, &msg, 0), EFAULT);
 
 	memset(recv_buffer, 0, sizeof(recv_buffer));
@@ -585,7 +541,6 @@ FN_TEST(sendmsg_and_recvmsg)
 		 _ret == strlen(good_buffer) - 1 &&
 			 memcmp(recv_buffer, good_buffer + 1,
 				strlen(good_buffer) - 1) == 0);
-#endif
 
 	// TEST CASE 5: Send a large buffer
 
